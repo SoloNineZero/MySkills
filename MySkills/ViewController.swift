15578d9028e1ba4b68e08c320b17088cbdef3b07
SoloNineZero <solodyankin.ie@ya.ru>
2023-08-01 23:29:52 +0500

    Заменены данные и иконки

--- MySkills/ViewController.swift
+++ MySkills/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    var editing: Bool
 
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    @IBOutlet var editButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+    }
+    
+    @IBAction func editButtonAction(_ sender: UIButton) {
+
+    }
+    
+    private func setupView() {
+        scrollView.bounces = false
+        scrollView.showsVerticalScrollIndicator = false
+        
     }
 
 
