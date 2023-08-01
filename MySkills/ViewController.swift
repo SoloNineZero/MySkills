d331afc9faffb64b54c0dcf93507ed292d0255ac
SoloNineZero <solodyankin.ie@ya.ru>
2023-08-01 21:49:20 +0500

    Initial Commit

---
+++ MySkills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySkills
+//
+//  Created by Игорь Солодянкин on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
